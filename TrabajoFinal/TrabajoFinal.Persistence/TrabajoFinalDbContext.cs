951c2aede010faacb5f9335e587696828bbd6d8e
gromero2095 <you@example.com>
2017-05-10 19:26:58 -0500

    Actualizacion

--- TrabajoFinal/TrabajoFinal.Persistence/TrabajoFinalDbContext.cs
+++ TrabajoFinal/TrabajoFinal.Persistence/TrabajoFinalDbContext.cs
@@ -12,5 +12,13 @@ namespace TrabajoFinal.Persistence
     {
         public DbSet<Autor> Autores { get; set; }
         public DbSet<Carrito> Carritos { get; set; }
-    }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Comprobante> Comprobantes { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Libreria> Librerias { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<LocalLibreria> LocalLibrerias { get; set; }
+        public DbSet<Nacionalidad> Nacionalidades { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+}
 }
